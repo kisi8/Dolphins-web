debae1b60df40a8da09557e30f8ff05cc0fe2eb4
Juraj Kyseľ <juraj.kisi.kysel@gmail.com>
2020-08-02 21:23:15 +0200

    Display homepage

--- dolphins_web/dolphins/views.py
+++ dolphins_web/dolphins/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("<h1>Nitra smrdí</h1>")
-    # return render(request, "home.html", data)
+    return render(request, "index.html")
